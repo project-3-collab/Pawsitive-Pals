9346d7078099f7548b94c3d9e56736e522817ca2
liliankim <yejean831@gmail.com>
2022-10-03 20:31:54 -0700

    added components and admin pages

---
+++ client/src/components/RequestQueue.js
@@ -0,0 +1,36 @@
+//request queue on sidebar that shows pending requests, approved and denied requests
+
+
+const RequestQueue = () => {
+
+  const ClickSingleRequest = (event) => {
+    console.log('clicked');
+  }
+
+  const ClickViewApproved = (event) => {
+    console.log('AllApproved');
+
+    const ClickViewDenied = (event) => {
+      console.log('AllDenied');
+        }
+
+  }
+
+
+  return (
+    <div>
+      <h1>Pawsitive Pals Requests</h1>
+      <container>View Individual Requests</container>
+      <ul>Open Requests</ul>
+      <li>Request 1</li>
+      <li>Request 2</li>
+      <li>Request 3</li>
+      <li>Request 4</li>
+      <li>Request 5</li>
+      <button>View Approved</button>
+      <button>View Denied</button>
+      </div>
+  )
+}
+export default RequestQueue;
+
---
+++ server/models/Request.js
@@ -0,0 +1,6 @@
+//requests from the user for pet play date
+//status can be completed,deleted,or pending
+//each button will apply a filter to array of requests
+//all requests will be pulled into here
+//user id ,pet id, date, and status
+
