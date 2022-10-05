d926671f444e40e0fc68a540037b71097038b0d5
Monineath Sar <monineathsar@gmail.com>
2022-10-04 20:08:24 -0700

    commenting out Lilian's admin page, to run app

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchPets from './pages/SearchPets';
 import SavedPets from './pages/SavedPets';
-
 import AdministratorRequests from './pages/AdministratorRequests';
-
 import PlaydateRequest from './components/PlaydateRequest';
-
 import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
